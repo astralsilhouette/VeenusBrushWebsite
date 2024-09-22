2b90022cd9f401326f41316aa823aa4a366f19bf
hackphynity <166407723+hackphynity@users.noreply.github.com>
2024-09-22 21:30:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Example to add interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
